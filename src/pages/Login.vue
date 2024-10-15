4e3cd494d61821c1ce850be832b04909e3de09b8
Laptop Thinkpad Gaga <gabrielferrero7306@gmail.com>
2024-10-15 17:31:13 +0200

    Tentativo di Procedura di login (nn funge)

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -2,19 +2,49 @@
     <div>
       <h1>Login Page</h1>
       <p>Pagina de login</p>
-      <form>
+      <form @submit.prevent="login">  
         <label for="username">Username:</label>
-        <input type="text" id="username" />
+        <input type="text" id="username" v-model="username"/>
         <label for="password">Password:</label>
-        <input type="password" id="password" />
+        <input type="password" id="password" v-model="password"/>
         <button type="submit">Login</button>
       </form>
     </div>
   </template>
   
   <script>
+  import { mapMutations } from 'vuex';
+
   export default {
-    name: 'LoginPage'
+    name: 'LoginPage',
+    
+    data: () => {
+      return {
+        username: "",
+        password: "",
+      };
+    },
+
+    methods: {
+      ...mapMutations(["setUser", "setToken"]),
+      async login() {
+        const response = fetch("http://localhost:8000/api/v1/auth/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: this.username,
+            password: this.password,
+          }),
+        });
+        
+        const { user, token } = await response.json();
+        this.setUser(user);
+        this.setToken(token);
+      },
+    },
+
   };
   </script>
   
