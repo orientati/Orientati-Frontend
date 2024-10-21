43060a80183fa775d2963b3a015d3e5c3100c156
gaga809 <gabrielferrero7306@gmail.com>
2024-10-21 19:12:31 +0200

    Logica per la gestione degli errori implementata

---
+++ js/error.js
@@ -0,0 +1,23 @@
+"use strict";
+window.addEventListener("load", init);
+
+function init(){
+    mostraErrore();
+}
+
+/**
+ * Mostra il messaggio e il codice di errore nella pagina
+ */
+function mostraErrore(){
+    let errorMsg = sessionStorage.getItem('errorMsg');
+    let messaggio = "Pagina inesistente", codice = 404;
+    
+    if(errorMsg){
+        errorMsg = JSON.parse(errorMsg);
+        messaggio = errorMsg.msg;
+        codice = errorMsg.code;
+    }
+
+    document.getElementById("txtMsg").innerText = messaggio;
+    document.getElementById("txtCodice").innerText = codice;
+}
