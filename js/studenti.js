c06abe1c211c6c30a9324394cb6d8ee2aae2e20a
andrea boasso <andreaboasso0@gmail.com>
2024-10-24 08:47:43 +0200

    creazione tabella studenti

--- js/studenti.js
+++ js/studenti.js
@@ -3,9 +3,31 @@
 window.addEventListener("DOMContentLoaded", function () {
   getStudenti()
     .then((data) => {
-      console.log(data);
+      createTable(data); 
     })
     .catch((err) => {
       mostraAlert("errore", err);
     });
 });
+
+function createTable(data) {
+  const table = document.getElementById("tableStudenti");
+  
+  data.forEach((student, index) => {
+    const row = document.createElement("tr");
+    
+    row.onclick = function() {
+      console.log("Student ID:", student.id); 
+    };
+    
+    row.innerHTML = `
+      <td>${index + 1}</td>
+      <td>${student.username}</td>
+      <td>${student.isAdmin ? "Yes" : "No"}</td>
+      <td>${student.isTemporary ? "Yes" : "No"}</td>
+      <td>${student.inGroup ? "Yes" : "No"}</td>
+    `;
+    
+    table.appendChild(row);
+  });
+}
