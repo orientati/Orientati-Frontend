041955a9c8a2424b0bf04f42d80280a654e5d14f
non laptop non thinkpad non di gaga <l.edmondo.2899@vallauri.edu>
2024-11-21 01:29:56 +0100

    implementazione parziale dashboard accompagnatori

--- js/pages/orientatore/dashboardOrientatore.js
+++ js/pages/orientatore/dashboardOrientatore.js
@@ -1,7 +1,45 @@
 "use strict"
+
 let gruppo;
 let data;
 let oraInizio;
+
+let tempoRimanente;
+let timerInterval = null;
+
+document.addEventListener("DOMContentLoaded", function () {
+    //aggiorna();
+    setInterval(aggiorna, 5000);
+});
+
+function aggiorna(){
+    setHead();
+}
+
+function avviaTimerInterval(){
+    if(timerInterval === null){
+        timerInterval = setInterval(aggiornaTimer, 1000);
+    }
+}
+
+function stoppaTimerInterval(){
+    if(timerInterval !== null){
+        clearInterval(timerInterval);
+        timerInterval = null;
+    }
+}
+
+function aggiornaTimer(){
+    tempoRimanente--;
+    if(tempoRimanente = 0){
+        clearInterval(timerInterval);
+        
+    }
+    let minuti = Math.floor(tempoRimanente / 60);
+    let secondi = tempoRimanente % 60;
+    console.warn(minuti + " - " + secondi);
+    //document.getElementById("tempo-rimanente").innerText = tempoRimanente + " minuti";
+}
 
 function setHead(){
     getGruppo().then((result) => {
@@ -25,6 +63,7 @@ function setHead(){
 
         document.getElementById("data").innerText = (data.toLocaleDateString("it-IT", {weekday:"long" ,day: "numeric", month: "numeric"})+" - "+data.toLocaleTimeString("it-IT", {hour: "2-digit", minute: "2-digit"})).toUpperCase();
         document.getElementById("nome-gruppo").innerText = result.gruppi[0].nome.toUpperCase();
+        setAula();
     }).catch((err) => {
         console.error(err);
     });
@@ -34,28 +73,55 @@ function setAula(){
     getTappe(gruppo.id).then((result) => {
         console.log(result);
 
-        let inizio = aggiungiMinuti(oraInizio, result.tappe[gruppo.numero_tappa].minuti_arrivo).toLocaleTimeString("it-IT", {hour: "2-digit", minute: "2-digit"});
-        let fine = aggiungiMinuti(oraInizio, result.tappe[gruppo.numero_tappa].minuti_partenza).toLocaleTimeString("it-IT", {hour: "2-digit", minute: "2-digit"});
-        console.log(inizio + " - " + fine);
-        document.getElementById("orari-teorico-attuale").innerText = inizio + " - " + fine;
+        if(gruppo.numero_tappa != 0){
+            let inizio = aggiungiMinuti(oraInizio, result.tappe[gruppo.numero_tappa-1].minuti_arrivo).toLocaleTimeString("it-IT", {hour: "2-digit", minute: "2-digit"});
+            let fine = aggiungiMinuti(oraInizio, result.tappe[gruppo.numero_tappa-1].minuti_partenza).toLocaleTimeString("it-IT", {hour: "2-digit", minute: "2-digit"});
+            console.log(inizio + " - " + fine);
+            document.getElementById("orari-teorico-attuale").innerText = inizio + " - " + fine;
+            document.getElementById("laboratorio-attuale").innerText = result.tappe[gruppo.numero_tappa-1].aula_materia.toUpperCase();
+            document.getElementById("aula-attuale").innerText = result.tappe[gruppo.numero_tappa-1].aula_posizione.toUpperCase() + "  " + result.tappe[gruppo.numero_tappa-1].aula_nome.toUpperCase();
+            if(timerInterval == null){
+                if(gruppo.arrivato)
+                    document.getElementById("azione-in-corso").innerText = "sei in:";
+                else
+                    document.getElementById("azione-in-corso").innerText = "in viaggio verso: ";
+
+                if(gruppo.arrivato){
+                    tempoRimanente = parseInt(result.tappe[gruppo.numero_tappa-1].minuti_partenza - result.tappe[gruppo.numero_tappa-1].minuti_arrivo)*60;
+                }else{
+                    if(!gruppo.numero_tappa == result.tappe.length && gruppo.numero_tappa != 0){
+                        tempoRimanente = parseInt(result.tappe[gruppo.numero_tappa].minuti_arrivo - result.tappe[gruppo.numero_tappa-1].minuti_partenza)*60;
+                    }else if(gruppo.numero_tappa == 0){
+                        tempoRimanente = parseInt(result.tappe[gruppo.numero_tappa].minuti_arrivo)*60;
+                    }
+                avviaTimerInterval();
+                }
+            }
+        }else{
+            document.getElementById("orari-teorico-attuale").innerText = "orario teorico partenza: "+ oraInizio.toLocaleTimeString("it-IT", {hour: "2-digit", minute: "2-digit"});
+            document.getElementById("laboratorio-attuale").innerText = "DEVI ANCORA PARTIRE";
+            document.getElementById("aula-attuale").innerText = "";
+        }
+        setProssimo(result);
     }).catch((err) => {
         console.error(err);
     });
 }
 
-function setProssimo(){
-    getTappe(gruppo.id).then((result) => {
-        console.log(result);
-
-        if(gruppo.numero_tappa+1){
-            let inizio = aggiungiMinuti(oraInizio, result.tappe[gruppo.numero_tappa+1].minuti_arrivo).toLocaleTimeString("it-IT", {hour: "2-digit", minute: "2-digit"});
-            let fine = aggiungiMinuti(oraInizio, result.tappe[gruppo.numero_tappa+1].minuti_partenza).toLocaleTimeString("it-IT", {hour: "2-digit", minute: "2-digit"});
-            console.log(inizio + " - " + fine);
-            document.getElementById("orari-teorico-futuro").innerText = inizio + " - " + fine;
-        }
-    }).catch((err) => {
-        console.error(err);
-    });
+function setProssimo(result){
+    console.warn(result);
+    if(!gruppo.numero_tappa == result.tappe.length){
+        let inizio = aggiungiMinuti(oraInizio, result.tappe[gruppo.numero_tappa].minuti_arrivo).toLocaleTimeString("it-IT", {hour: "2-digit", minute: "2-digit"});
+        let fine = aggiungiMinuti(oraInizio, result.tappe[gruppo.numero_tappa].minuti_partenza).toLocaleTimeString("it-IT", {hour: "2-digit", minute: "2-digit"});
+        console.log(inizio + " - " + fine);
+        document.getElementById("orari-teorico-futuro").innerText = inizio + " - " + fine;
+        document.getElementById("laboratorio-futuro").innerText = result.tappe[gruppo.numero_tappa].aula_materia.toUpperCase();
+        document.getElementById("aula-futura").innerText = result.tappe[gruppo.numero_tappa].aula_posizione.toUpperCase() + "  " + result.tappe[gruppo.numero_tappa].aula_nome.toUpperCase();
+    }else{
+        document.getElementById("orari-teorico-futuro").innerText = "arrivo previsto: "+ aggiungiMinuti(oraInizio, result.tappe[gruppo.numero_tappa-1].minuti_partenza).toLocaleTimeString("it-IT", {hour: "2-digit", minute: "2-digit"});
+        document.getElementById("laboratorio-futuro").innerText = "FINE PERCORSO";
+        document.getElementById("aula-futura").innerText = "";
+    }
 }
 
 function aggiungiMinuti(data, minuti) {
