c6315d1718ba13caddc8b0d0a7880f8c1d4e64ae
gaga809 <gabrielferrero7306@gmail.com>
2024-11-21 18:35:46 +0100

    Aggiunta logica aggiornamento in tempo reale dei gruppi

--- js/controllers/dashboardAdminController.js
+++ js/controllers/dashboardAdminController.js
@@ -11,9 +11,13 @@ function getGruppi() {
 
     vallauriRequest(`${serverUrl}admin/dashboard/gruppi`, "GET", headers)
       .then((response) => {
+        console.log(response);
         gruppi = response.gruppi;
 
-        for (let i = 0; i<gruppi.length; i++) {
+        const aulaDet = getAulaFromTappa(gruppi[i].numero_tappa);
+        gruppi.aula = aulaDet;
+
+        for (let i = 0; i < gruppi.length; i++) {
           vallauriRequest(
             `${serverUrl}admin/gruppi/tappe/${gruppi[i].id}`,
             "GET",
@@ -38,29 +42,52 @@ function getGruppi() {
 
 function findNextTappa(tappe, tappaId) {
   let i;
-  for (i=0;i<tappe.length;i++) {
+  for (i = 0; i < tappe.length; i++) {
     if (tappe[i].id == tappaId) break;
   }
 
-  if (i < tappe.length) return tappe[i];
+  if (i < tappe.length){
+    const aulaDet = getAulaFromTappa(tappe[i].id);
+    tappe[i].aula = aulaDet;
+    return tappe[i];
+  } 
   else return null;
 }
 
-function getOrientati(){
-    return new Promise((res, rej) => {
-        const access_token = localStorage.getItem("access_token");
-        const headers = {
-          Authorization: `Bearer ${access_token}`,
-        };
-    
-        vallauriRequest(`${serverUrl}admin/dashboard/orientati`, "GET", headers)
-          .then((response) => {
-            orientati = response.orientati;
-            res(orientati);
-          })
-          .catch((err) => {
-            console.error(err);
-            rej("Errore nella ricezione degli orientati");
-          });
+function getAulaFromTappa(tappaId){
+  const headers = {
+    Authorization: `Bearer ${access_token}`,
+  };
+
+  vallauriRequest(`${serverUrl}admin/tappe/${tappaId}`, "GET", headers)
+  .then(tappa => {
+    vallauriRequest(`${serverUrl}admin/aule/${tappa.aula_id}`)
+    .then(aula => {
+      return aula;
+    }).catch(()=>{
+      return null;
+    })
+
+  }).catch(()=>{
+    return null;
+  })
+}
+
+function getOrientati() {
+  return new Promise((res, rej) => {
+    const access_token = localStorage.getItem("access_token");
+    const headers = {
+      Authorization: `Bearer ${access_token}`,
+    };
+
+    vallauriRequest(`${serverUrl}admin/dashboard/orientati`, "GET", headers)
+      .then((response) => {
+        orientati = response.orientati;
+        res(orientati);
+      })
+      .catch((err) => {
+        console.error(err);
+        rej("Errore nella ricezione degli orientati");
       });
+  });
 }
