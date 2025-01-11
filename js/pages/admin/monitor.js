f7ecf2685f9d7c4714360bcd611a85712b8b802f
Pietro <p.audifreddi.2619@vallauri.edu>
2025-01-11 10:33:59 +0100

    Update: convertito da dinamico in statico per risolvere il flicker delle progress bar

--- js/pages/admin/monitor.js
+++ js/pages/admin/monitor.js
@@ -50,31 +50,27 @@ function loadGruppi(groups) {
 }
 
 function creaGruppo(group, aula) {
-    divGruppo.innerHTML = "";
 
-    const nomeGruppo = document.createElement("h1");
-    nomeGruppo.innerHTML = "Gruppo <span>" + group.nome + "</span>";
+    document.getElementById("nomeGruppo").innerHTML = group.nome;
 
-    const orariTeorici = document.createElement("p");
-    orariTeorici.textContent = group.orario_partenza;
-
-    const divTop = document.createElement("div");
-    divTop.appendChild(nomeGruppo);
-    divTop.appendChild(orariTeorici);
-
-    const orariReali = document.createElement("h2");
     let [ore, minuti] = group.orario_partenza.split(":").map(Number);
     let orarioFineTeorico = new Date();
     orarioFineTeorico.setHours(ore);
     orarioFineTeorico.setMinutes(minuti);
     orarioFineTeorico.setSeconds(0);
     orarioFineTeorico.setMinutes(orarioFineTeorico.getMinutes() + 10);
-    orariTeorici.textContent += " - " + String(orarioFineTeorico.getHours()).padStart(2, "0") + ":" + String(orarioFineTeorico.getMinutes()).padStart(2, "0");
+    document.getElementById("orariTeorici").textContent = group.orario_partenza + " - " + String(orarioFineTeorico.getHours()).padStart(2, "0")
+        + ":" + String(orarioFineTeorico.getMinutes()).padStart(2, "0");
+
+    document.getElementById("presenza").textContent = group.orientati_presenti + "/" + (group.totale_orientati - group.orientati_assenti);
 
     let orarioFineReale;
 
+    document.getElementById("orariReali").textContent = group.nome;
+
     if (group.numero_tappa === 0) {
-        orariReali.textContent = "Non partito";
+        document.getElementById("orariReali").textContent = "Non partito";
+        document.getElementsByClassName("outer-div-slider")[1].classList.add("hide");
     } else {
         [ore, minuti] = group.orario_partenza_effettivo.split(":").map(Number);
         orarioFineReale = new Date();
@@ -82,59 +78,29 @@ function creaGruppo(group, aula) {
         orarioFineReale.setMinutes(minuti);
         orarioFineReale.setSeconds(0);
         orarioFineReale.setMinutes(orarioFineReale.getMinutes() + 10);
-        orariReali.textContent = group.orario_partenza_effettivo + " - " + String(orarioFineReale.getHours()).padStart(2, "0") + ":" + String(orarioFineReale.getMinutes()).padStart(2, "0");
+        document.getElementById("orariReali").textContent = group.orario_partenza_effettivo + " - " + String(orarioFineReale.getHours()).padStart(2, "0") + ":" + String(orarioFineReale.getMinutes()).padStart(2, "0");
+        document.getElementsByClassName("outer-div-slider")[1].classList.remove("hide");
     }
-    const divBottom = document.createElement("div");
-    const text = document.createElement("p");
-    text.textContent = "Orari reali:";
 
-
-    divBottom.appendChild(text);
-    divBottom.appendChild(orariReali);
-
-    const outerDivSliderTeorico = document.createElement("div");
-    outerDivSliderTeorico.classList.add("outer-div-slider");
-
-    const sliderTeorico = document.createElement("progress");
-    sliderTeorico.classList.add("slider");
-    sliderTeorico.max = 100;
-    sliderTeorico.value = 0;
-    outerDivSliderTeorico.appendChild(sliderTeorico);
-
-    const outerDivSliderReale = document.createElement("div");
-    outerDivSliderReale.classList.add("outer-div-slider");
-
-    const sliderReale = document.createElement("progress");
-    sliderReale.classList.add("slider");
-    sliderReale.max = 100;
-    sliderReale.value = 0;
-    outerDivSliderReale.appendChild(sliderReale);
-
+    document.getElementById("text").textContent = "Orari reali:";
 
     const tempoTotaleMinuti = 10; //TODO: DA RICAVARE
-    setInterval(aggiornaSlider, 50);
+    setInterval(aggiornaSlider, 1000);
 
     function aggiornaSlider() {
         const oraAttuale = new Date();
 
         const differenzaMinutiTeorico = Math.max(0, (orarioFineTeorico - oraAttuale) / (1000 * 60));
         const percentualeCompletamentoTeorico = Math.min(100, ((tempoTotaleMinuti - differenzaMinutiTeorico) / tempoTotaleMinuti) * 100);
-        sliderTeorico.value = 100 - percentualeCompletamentoTeorico;
+        document.getElementById("progressTeorico").value = 100 - percentualeCompletamentoTeorico;
 
         if (group.numero_tappa !== 0) {
             const differenzaMinutiReale = Math.max(0, (orarioFineReale - oraAttuale) / (1000 * 60));
             const percentualeCompletamentoReale = Math.min(100, ((tempoTotaleMinuti - differenzaMinutiReale) / tempoTotaleMinuti) * 100);
-            sliderReale.value = 100 - percentualeCompletamentoReale;
+
+            document.getElementById("progressReale").value = 100 - percentualeCompletamentoReale;
         }
     }
-
-    divTop.appendChild(outerDivSliderTeorico);
-    if (group.numero_tappa !== 0) {
-        divBottom.appendChild(outerDivSliderReale);
-    }
-
-    divGruppo.appendChild(divTop);
-    divGruppo.appendChild(divBottom);
 
     //Chimica
     const lab = document.getElementById("lab");
@@ -145,7 +111,6 @@ function creaGruppo(group, aula) {
     availability.textContent = aula.occupata ? "OCCUPATO" : "LIBERO";
     availability.classList.add(aula.occupata ? "occupato" : "libero");
     availability.classList.remove(!aula.occupata ? "occupato" : "libero");
-
 }
 
 
