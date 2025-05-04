503fba30bc0b2099220e92501f54155acc7d25b9
Pietro Audifreddi <p.audifreddi.2619@vallauri.edu>
2025-05-04 17:04:13 +0200

    Update: riformattato il progetto, aggiunto token ed api service e token interceptor per la gestione di request e access token

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,3 @@
 import { Routes } from '@angular/router';
-import {RowComponent} from "./row/row.component";
-import {DashboardPageComponent} from "./dashboard-page/dashboard-page.component";
 
-export const routes: Routes = [
-    {path: '', component: DashboardPageComponent},
-];
+export const routes: Routes = [];
---
+++ src/app/core/services/token/token.service.ts
@@ -0,0 +1,48 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {map, catchError} from 'rxjs/operators';
+
+@Injectable({providedIn: 'root'})
+export class TokenService {
+  private readonly API_URL = ''; //TODO
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAccessToken(): string | null {
+    return localStorage.getItem('accessToken');
+  }
+
+  getRefreshToken(): string | null {
+    return localStorage.getItem('refreshToken');
+  }
+
+  saveTokens(access: string, refresh: string): void {
+    localStorage.setItem('accessToken', access);
+    localStorage.setItem('refreshToken', refresh);
+  }
+
+  refreshToken(): Observable<string> {
+    const refreshToken = this.getRefreshToken();
+    if (!refreshToken) return throwError(() => new Error('No refresh token'));
+
+    return this.http.post<any>(`${this.API_URL}/refresh-token`, {
+      refreshToken: refreshToken
+    }).pipe(
+      map(response => {
+        this.saveTokens(response.accessToken, response.refreshToken);
+        return response.accessToken;
+      }),
+      catchError(err => {
+        return throwError(() => err);
+      })
+    );
+  }
+
+  logout(): void {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+    location.href = ''; //TODO
+  }
+}
